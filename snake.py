e39c8892038a168c962e7eb6f4781a3dad546988
EgievOmar <omar.egiev@epitech.eu>
2023-04-04 14:19:46 +0200

    all the basics initialisations made

--- snake.py
+++ snake.py
@@ -1,19 +1,47 @@
-import curses
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+pygame.init()
 
-#setup the game window
-curses.initscr() 
-win = curses.newwin(20, 60, 0, 0)
-win.keypad(1)
-curses.noecho()
-curses.curs_set(0)
-win.border(0)
-win.nodelay(1)
+class Direction(Enum) :
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
 
-#game program
-score = 0
+Point = namedtuple('Point', 'x', 'y')
 
-while True: 
-    even = win.getch()
+BLOCK_SIZE = 20
+class SnakeGame :
+    
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+    
+        #init screen
+        self.display = pygame.display.set_mode((self.w, self.h)) # =tuple : list filled with constant, make sure that the values doesn't changes (security)
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
 
-curses.endwin()
-print(f"Final score {score}") 
+        #init game state
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head, 
+                      Point(self.head.x - BLOCK_SIZE, self.head.y), 
+                      Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)]
+ 
+        self.score = 0
+        self.food = None
+
+    def play_step(self):
+        pass
+
+if __name__ == '_main_':
+    game = SnakeGame()
+
+    while True:
+        game.play_step()
+    
+    pygame.quit()
